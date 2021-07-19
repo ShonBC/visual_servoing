33d635c3809339c47973d8913c03d7bf3a98fd9d
SouvikUMD <87155394+SouvikUMD@users.noreply.github.com>
2021-07-19 00:18:00 -0400

    Modifications. Please add to this.

--- final_project_group2/scripts/follower.py
+++ final_project_group2/scripts/follower.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 
-
+from scripts import leader
+import os
 import rospy
+import leader.Leader
 import actionlib
 import sys
+import tf
 from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
+from bot_controller.util import compute_distance
 from tf.transformations import euler_from_quaternion
 from math import radians, degrees, atan2, pi
+from sensor_msgs.msg import LaserScan
 
+import src.move_base_example
+import src.spawn_marker_on_robot
 
-class Follower(object):
-    pass
+class Follower(leader.Leader):
+    def __init__(self, rate=4):
+        super().__init__(self)
+        self._safe_dist = 0.5
 
 
 if __name__ == '__main__':
-    rospy.init_node('movebase_client_leader')
+    try:
+        Follower = Follower()
+        Follower.handle_inputs()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Action terminated.")
+    rospy.init_node('node_name')

65de92e3918a0c6f2cf913a8ff8b42ffb8070a06
f18pilot <47767247+f18pilot@users.noreply.github.com>
2021-07-21 09:38:25 -0400

    Update read_waypts_yaml.py
    clean up of all PEP8 comments and documentation checked

--- final_project_group2/src/read_waypts_yaml.py
+++ final_project_group2/src/read_waypts_yaml.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
+
 """
 This Python file will take the information from "waypoints.yml" and convert the information to a data structure which
 will be further used for ROS commands.
-
 """
 import os
 
-robot_waypts = {}
+robot_waypoints = {}
+
 
 def file_path():
     """Find file path for yaml file to read
@@ -19,6 +21,7 @@ def file_path():
     parent_dir = os.path.dirname(file_dir)
     new_path = os.path.join(parent_dir, 'yaml')
     return new_path
+
 
 def read_file(path):
     """
@@ -35,7 +38,6 @@ def read_file(path):
 
         for line in lines:
             state_list.append(line)
-            # print(line)
 
         return state_list
 
@@ -43,35 +45,32 @@ def read_file(path):
 def create_commands():
     """
     This function will return a dictionary containing all of the commands from waypoints.yaml.
-    :return: dictionary robot_waypts
+    
+    :return: dictionary robot_waypoints
+    
     """
     path = file_path()
     initial_list = read_file(path + '/' + 'waypoints.yaml')
     count = 0
+    section = ""
     sub_section = ""
     for elt in initial_list:
         new_elt = (" ".join(elt.split())).replace(":", "")
-        print(new_elt)
         if "orientation" in elt or "w" in elt or "x" in elt or "y" in elt or "z" in elt or "position" in elt:
             if new_elt == "orientation" or new_elt == "position":
-                robot_waypts[section][new_elt] = {}
+                robot_waypoints[section][new_elt] = {}
                 sub_section = new_elt
             else:
-                # if "w" in new_elt:
-                #     robot_waypts[section]["orientation"]["w"] = new_elt[2:]
-                # else:
-                robot_waypts[section][sub_section][new_elt[0]] = new_elt[2:]
+                robot_waypoints[section][sub_section][new_elt[0]] = new_elt[2:]
 
         else:
             count += 1
             section = (count, new_elt)
-            robot_waypts[section] = {}
-                        
-            
-    return robot_waypts
+            robot_waypoints[section] = {}
+
+    return robot_waypoints
 
 
 if __name__ == "__main__":
     
-    robot_cmds = create_commands()
-    print(robot_cmds)
+    robot_goals = create_commands()
